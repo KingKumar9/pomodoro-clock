811d1d988e6221e4174acd3a6ad0a5a705575e7f
Rajat Kumar <rajatk12199@gmail.com>
2021-12-07 20:41:50 +1100

    cleanup and setup changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
+import React from 'react'
 import './App.css';
 
-function App() {
-  return (
-    <div className="container">
-      
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="container">
+
+      </div>
+    )
+  }
 }
 
 export default App;
